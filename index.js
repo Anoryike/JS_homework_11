2ae3274cc7643f799fe1d3950c5fb14e5dfc0b5f
Anoryike <149113497+Anoryike@users.noreply.github.com>
2024-02-24 22:52:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// import { name } from "./module";
+// import "./style.css";
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+
+// console.log("Your name: " + name);
+
+// console.log(isEmail("foo@bar.com"));
+// console.log(isURL("123"));
+
+// var moment = require("moment");
+
+// console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
